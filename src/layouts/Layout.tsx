219d68f61f59d31ff3afcf5b7cfb93392c02cd49
김연진 <kyj0719@gmail.com>
2025-10-16 12:17:39 +0900

    ✨feat: 라이브러리 버전 업 및 allowInProd 허용

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { ThemeProvider, ThemeSwitcher } from "colbrush/client";
+
+import { SimulationFilter } from "colbrush/devtools";
+import { ThemeSwitcher } from "colbrush/client";
 
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Header />
       <main className="flex-1">
-        <ThemeProvider>
-          <Outlet />
-          <ThemeSwitcher />
-        </ThemeProvider>
+        <Outlet />
+        <SimulationFilter allowInProd={true} />
+        <ThemeSwitcher />
       </main>
       <Footer />
     </div>
